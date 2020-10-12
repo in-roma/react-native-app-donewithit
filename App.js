0021678d60c7466eaeb6140b482a7f7586860230
in-roma <romain.grandjean@outlook.com>
2020-10-12 09:46:51 +0200

    new component screen

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { StyleSheet, SafeAreaView, View } from 'react-native';
 
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import AppText from './app/components/AppText';
 import AppButton from './app/components/AppButton';
 
 export default function App() {
-	return <WelcomeScreen />;
+	return <ListingDetailsScreen />;
 }
