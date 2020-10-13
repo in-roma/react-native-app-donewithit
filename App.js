4fd553284501a4dc428111996e1cb1a89eff40c9
in-roma <romain.grandjean@outlook.com>
2020-10-13 15:20:00 +0200

    login screen done

--- App.js
+++ App.js
@@ -1,40 +1,8 @@
 import React, { useState } from 'react';
 import { StyleSheet, SafeAreaView, View } from 'react-native';
 
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
-
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import ListItem from './app/components/ListItem';
-import AccountScreen from './app/screens/AccountScreen';
-
-import Screen from './app/components/Screen';
-import AppText from './app/components/AppText';
-import AppTextInput from './app/components/AppTextInput';
-import AppPicker from './app/components/AppPicker';
-import AppButton from './app/components/AppButton';
-import Icon from './app/components/Icon';
-
-const categories = [
-	{ label: 'furniture', value: 1 },
-	{ label: 'Clothing', value: 2 },
-	{ label: 'Cameras', value: 3 },
-];
+import LoginScreen from './app/screens/LoginScreen';
 
 export default function App() {
-	const [category, setCategory] = useState(categories[0]);
-	return (
-		<Screen>
-			<AppPicker
-				selectedItem={category}
-				onSelectItem={(item) => setCategory(item)}
-				icon="apps"
-				items={categories}
-				placeholder="Category"
-			/>
-			<AppTextInput icon="email" placeholder="Email" />
-		</Screen>
-	);
+	return <LoginScreen />;
 }
