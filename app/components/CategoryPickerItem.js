d484b8e05cce4dd1de37cfb6f845114db4fdb54d
in-roma <romain.grandjean@outlook.com>
2020-10-15 11:11:50 +0200

    new components + location custom hooks

--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from './Icon';
 import AppText from './AppText';
 
 function CategoryPickerItem({ onPress, item }) {
 	return (
 		<View style={styles.container}>
-			<Icon
-				backgroundColor={item.backgroundColor}
-				name={item.icon}
-				size={80}
-			/>
+			<TouchableOpacity onPress={onPress}>
+				<Icon
+					backgroundColor={item.backgroundColor}
+					name={item.icon}
+					size={80}
+				/>
+			</TouchableOpacity>
 			<AppText style={styles.label}>{item.label}</AppText>
 		</View>
 	);
--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import * as Yup from 'yup';
 
+import useLocation from '../hooks/useLocation';
 import CategoryPickerItem from '../components/CategoryPickerItem';
 import Screen from '../components/Screen';
 import {
@@ -10,12 +11,14 @@ import {
 	SubmitButton,
 	AppFormPicker,
 } from '../components/forms';
+import AppFormImagePicker from '../components/forms/AppFormImagePicker';
 
 const validationSchema = Yup.object().shape({
 	title: Yup.string().required().min(1).label('Title'),
 	price: Yup.number().required().min(1).max(10000).label('Price'),
 	description: Yup.string().label('Description'),
 	category: Yup.object().required().nullable().label('Category'),
+	images: Yup.array().min(1, 'Please select at least one image'),
 });
 
 const categories = [
@@ -76,6 +79,8 @@ const categories = [
 ];
 
 function ListingEditScreen(props) {
+	const location = useLocation();
+
 	return (
 		<Screen>
 			<AppForm
@@ -84,10 +89,12 @@ function ListingEditScreen(props) {
 					price: '',
 					description: '',
 					category: null,
+					images: [],
 				}}
-				onSubmit={(values) => console.log(values)}
+				onSubmit={(values) => console.log(location)}
 				validationSchema={validationSchema}
 			>
+				<AppFormImagePicker name="images" />
 				<AppFormField
 					maxLength={255}
 					name="title"
