30f63b6c13c447d55a2a63d53b11ff16c4bf54ed
in-roma <romain.grandjean@outlook.com>
2020-10-15 09:34:01 +0200

    ImageInput component done

--- App.js
+++ App.js
@@ -29,12 +29,10 @@ export default function App() {
 	};
 	return (
 		<Screen>
-			<Button title="Select Image" onPress={selectImage} />
-			<Image
-				source={{ uri: imageUri }}
-				style={{ width: 200, height: 200 }}
+			<ImageInput
+				imageUri={imageUri}
+				onChangeImage={(uri) => setImageUri(uri)}
 			/>
-			<ImageInput imageUri={imageUri} />
 		</Screen>
 	);
 }
